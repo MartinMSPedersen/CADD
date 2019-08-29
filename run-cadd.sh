87735c98383568299436e7b7b8b00bb9acf7097e
Martin Møller Skarbiniks Pedersen <traxplayer@gmail.com>
2019-08-29 09:51:26 +0200

    Update run-cadd.sh

--- run-cadd.sh
+++ run-cadd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --mount type=bind,source=$PWD/data,target=/cadd-src/CADD-scripts-master/data cadd /bin/bash
+docker container run -it --mount type=bind,source=$PWD/data,target=/cadd-src/CADD-scripts-master/data cadd /bin/bash
 
